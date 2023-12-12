5b1409be4eec05ba794a79109727f61b9e67d354
narendrapb <119730895+narendrapb@users.noreply.github.com>
2023-12-12 13:12:17 +0530

    Update exapmle.py

--- exapmle.py
+++ exapmle.py
@@ -2,6 +2,4 @@ sfhdgcvmb,jnkluytdhgfvbc
 sfdgv gsxvgfvbnjy
 hi i am narendra
 just uploading example.pyfghmbj,mbjh nyhgbh ng bv
-jcij fcifk weicm kj,hnc kmbkjckja
-wafserklgmkrivkgjjkkd
-dgfgh
+jcij fcifk weicm kj,hnc kmbkjckjadgfgh
